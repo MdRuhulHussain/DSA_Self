4c090f9cf0379e9dc90a3fa506f6cded1cd288d1
Mohammed Ruhul Hossain <Mohammed.Hossain@in.ey.com>
2023-09-28 21:14:24 +0530

    Printing various patterns

---
+++ src/dsa/basics/PatternPrint.java
@@ -0,0 +1,55 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class PatternPrint {
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please provide the size of the triangle, eg - 5");
+		int n = sc.nextInt();
+		
+/* printing shape 
+ 		*****
+ 		****
+ 		***
+ 		**
+ 		*
+*/
+
+		for(int i = 1;i<=n;i++) {
+			for(int j=n;j>=i;j--) 
+				System.out.print("*");
+			System.out.println();
+		}
+
+/*
+ 		*
+ 		**
+ 		***
+ 		****
+ 		*****
+*/
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) 
+				System.out.print("*");
+			System.out.println();
+		}
+		
+/*
+ 		A
+		BB
+		CCC
+		DDDD
+		EEEEE
+ */
+		char ch = 'A';
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++)
+				System.out.print(ch);
+			ch++;
+			System.out.println();
+		}
+	}
+
+}
