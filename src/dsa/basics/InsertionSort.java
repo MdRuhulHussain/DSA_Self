19529049b9cddccab2bd2764363ee1bf17ee4d32
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-10-11 01:58:31 +0530

    Insertion Sort added

--- src/dsa/basics/InsertionSort.java
+++ src/dsa/basics/InsertionSort.java
@@ -1,5 +1,12 @@
 package dsa.basics;
 
+
+/*
+ * To sort an array of size N in ascending order iterate over the array and compare 
+ * the current element (key) to its predecessor, if the key element is smaller than
+ *  its predecessor, compare it to the elements before. Move the greater elements 
+ *  one position up to make space for the swapped element.
+ */
 public class InsertionSort {
 	
 	public static void main(String args[]) {
