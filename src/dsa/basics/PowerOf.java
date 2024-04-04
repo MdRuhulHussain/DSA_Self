cd66ab7f83ee1a253a3352864f5a6a424b2a90e0
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-04-05 00:25:53 +0530

    PowerOf x to n

--- src/dsa/basics/PowerOf.java
+++ src/dsa/basics/PowerOf.java
@@ -14,35 +14,49 @@ public class PowerOf {
 
 	public static void main(String args[]) {
 		
-		System.out.println(powerUsingRecursion(2,39));
+		System.out.println(powerUsingRecursion(2,-3));
 		
-		System.out.println(powerUsingLoop(2,39));
+		System.out.println(powerUsingLoop(2,-3));
 	}
 	
-	public static long powerUsingRecursion(int a, int b) {
+	public static double powerUsingRecursion(double a, int b) {
 //		System.out.println(b);
 		if(b==0)
 			return 1;
 		if(b==1)
 			return a;
-		long prod = powerUsingRecursion(a, b/2);
-		
+		if(b==-1) 
+			return 1/a;
+
+			
+		double prod = powerUsingRecursion(a, b/2);
+		System.out.println(b+"a ="+a+" prod "+prod);
 		if(b%2==0)
 			return prod*prod;
+		else if(b<0)
+			return 1/a*prod*prod;
 		else
 			return a*prod*prod;
 	}
 	
 	//below is the example of executing the same using loop, but this solution takes more space complexity
 	//because of storing list integer.
-	public static long powerUsingLoop(int a, int b) {
+	public static double powerUsingLoop(int a, int b) {
 		
 		List<Integer> li = new ArrayList<>();
-		while(b>1) {
-			li.add(b);
-			b/=2;
+		//min val of integer 2^31, while max val of integer is 2^31-1, so when we will use 
+		//Math.abs(-2147483648), it won't give us 2147483648 and hence will give wrong output
+		//to handle this case explicitly I'm storing it. and then going for next val of it, i.e b/2
+		if(b==-2147483648){ 
+            b/=2;
+            li.add(b);
+        }
+		int pow = Math.abs(b);
+		while(pow>1) {
+			li.add(pow);
+			pow/=2;
 		}
-		long prod = a;
+		double prod = a;
 		for(int i=li.size()-1;i>=0;i--) {
 //			System.out.println(li.get(i));
 			if(li.get(i)%2==0)
@@ -51,22 +65,9 @@ public class PowerOf {
 				prod=a*prod*prod;
 			
 		}
+		if(b<0)
+			prod=1/prod;
 		return prod;
-//		int prod = b%2==0?a:1;
-//		int temp = b;
-//		while(b>0) {
-////			if(b==1)
-////				return prod*=a;
-//			if(b%2==0)
-//				prod*=prod;
-//			else if(temp%2==0 && b==1)
-//				return prod;
-//			else
-//				prod*=a*prod;
-//			b/=2;
-//		}
-//		
-//		return prod;
 		
 	}
 }
